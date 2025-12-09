8fe4088e407cb972e139108365a760c60b3e3c4b
Kostia Hryhorov <kgraven2009@gmail.com>
2025-12-09 15:42:35 +0200

    словник

---
+++ task_1.py
@@ -0,0 +1,13 @@
+book_profile={
+    "title": "Основи програмуваня" ,
+    "author": "Іван Петренко" ,
+    "year": 2023,
+    "publisher_info":{
+        "name": "наукова думка",
+        "city":"Київ"
+    }
+}
+print(book_profile["title"])
+print(book_profile["author"])
+print(book_profile["publisher_info"]["name"])
+print()
